83af3228691de9fd40730a04440591b350e88964
Xu Yun <yun.y.xu@gmail.com>
2023-03-12 18:48:10 +0800

    Do some refactories

--- translate_chn.py
+++ translate_chn.py
@@ -7,7 +7,7 @@ import argparse
 
 token_encoding = tiktoken.get_encoding("cl100k_base")
 
-def set_openapi_conf(api_key, api_base):
+def set_openapi_conf(api_key: str, api_base: str) -> None:
     if api_key:
         openai.api_key = api_key
     if api_base:
@@ -33,7 +33,7 @@ def set_openapi_conf(api_key, api_base):
             if os.path.exists(expected_apibase_filename):
                 openai.api_base = open(expected_apibase_filename).read().strip()
 
-def ask(user_text, temperature):
+def ask(user_text: str, temperature: float) -> tuple[str, int]:
     MODEL = "gpt-3.5-turbo"
     response = openai.ChatCompletion.create(
       model = MODEL,
@@ -47,17 +47,17 @@ def ask(user_text, temperature):
       timeout = 120,
       temperature = temperature
     )
-    response_text = response.choices[0].message.content
-    total_tokens = response.usage.total_tokens
+    response_text:str = response.choices[0].message.content
+    total_tokens:int = response.usage.total_tokens
     return response_text, total_tokens
 
 # def ask(user_text, temperature):
 #     return user_text, get_tokens(user_text)
 
-def get_tokens(text):
+def get_tokens(text: str) -> int:
     return len(token_encoding.encode(text))
 
-def get_next_split(text, start_pos, max_tokens=1365):
+def get_next_split(text: str, start_pos: int, max_tokens=1365) -> tuple[str, int, int, bool]:
     sentence_split_punctuation = '.!?'
     # Let's assume that each token has 6 character length on average
     end_pos = min(len(text) - 1, start_pos + max_tokens * 6)
@@ -70,7 +70,7 @@ def get_next_split(text, start_pos, max_tokens=1365):
                 done = True
             return ret, end_pos + 1, tokens, done
         # search backwards for a sentence
-        while True and end_pos > 0:
+        while end_pos > 0:
             end_pos -= 1
             c = text[end_pos]
             if c in sentence_split_punctuation:
@@ -80,7 +80,9 @@ def translate(
         source_english_filename: str, 
         output_chinese_filename: str,
         start_split_no=1,
-        translate_limits=65535) -> None:
+        splits_to_translate=65535,
+        temperature=0.6,
+        max_token_per_request=1365) -> None:
     text = open(source_english_filename, encoding="utf-8").read()
 
     done = False
@@ -94,13 +96,13 @@ def translate(
         splited_text, start_pos, _, done = get_next_split(text, start_pos)
         current_split_no += 1
     try:
-        while not done and translated_splits < translate_limits:
-            splited_text, start_pos, _, done = get_next_split(text, start_pos)
+        while not done and translated_splits < splits_to_translate:
+            splited_text, start_pos, _, done = get_next_split(text, start_pos, max_tokens=max_token_per_request)
             if not splited_text.strip():
                 continue
             start_time = time.time()
             print(f'Request to translate split: {current_split_no}...', end='', flush=True)
-            ret, total_tokens = ask(splited_text, 0.6)
+            ret, total_tokens = ask(splited_text, temperature)
             current_split_no += 1
             translated_splits += 1
             total_tokens_used += total_tokens
@@ -119,7 +121,7 @@ def translate(
     except KeyboardInterrupt:
         print('break')
     finally:
-        print(f'Stopped at split {current_split_no}, finished {translate_limits} splits')
+        print(f'Stopped at split {current_split_no}, finished {splits_to_translate} splits')
         print(f'Token used: {total_tokens_used}, ${total_tokens_used / 1000 * 0.002:.3f} dollars.')
 
 def get_arguments():
@@ -131,15 +133,19 @@ def get_arguments():
     parser.add_argument('--start', help='The split number to start', type=int, default=1)
     parser.add_argument('--splits', help='The number of splits to be translated', type=int, default=65536)
     parser.add_argument('--temperature', help="API's temperature parameter (0~2.0)", type=float, default=0.6)
+    parser.add_argument('--tokens', help='Maximum number of English tokens in one API call', type=int, default=1365)
 
     args = parser.parse_args()
     return args
 
-arg = get_arguments()
-set_openapi_conf(arg.id, arg.api)
-translate(
-    source_english_filename=arg.src,
-    output_chinese_filename=arg.output,
-    start_split_no=arg.start,
-    translate_limits=arg.splits
-)
+if __name__ == '__main__':
+    arg = get_arguments()
+    set_openapi_conf(arg.id, arg.api)
+    translate(
+        source_english_filename=arg.src,
+        output_chinese_filename=arg.output,
+        start_split_no=arg.start,
+        splits_to_translate=arg.splits,
+        temperature=arg.temperature,
+        max_token_per_request=arg.tokens,
+    )
